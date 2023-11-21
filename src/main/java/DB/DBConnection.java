3b659433672ae92a2c19311f560ed181cb50a568
ThangPham <phamthanglg2020@gmail.com>
2023-11-21 10:58:05 +0700

    Them giao dien login register

---
+++ src/main/java/DB/DBConnection.java
@@ -0,0 +1,28 @@
+package DB;
+import java.sql.*;
+public class DBConnection {
+
+	public static Connection CreateConnection() {
+		Connection conn = null;
+		String url = "jdbc:mysql://127.0.0.1:3306/wish?useUnicode=true&characterEncoding=utf8";
+		String username = "root";
+		String password = "huythang02";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// create connection
+			conn = DriverManager.getConnection(url, username, password);
+//			conn.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+}
